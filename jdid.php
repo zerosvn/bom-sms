a6772de31b59c24648357a08abad730f14900d34
faridfac <muhammadfarid.alfachri@gmail.com>
2019-10-26 18:08:54 +0700

    Update jdid.php

--- jdid.php
+++ jdid.php
@@ -16,7 +16,7 @@ function jdidbom($no, $jum, $wait){
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($ch, CURLOPT_REFERER, 'http://sc.jd.id/phone/bindingPhone.html');
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36');
         $server_output = curl_exec ($ch);
         curl_close ($ch);
 		echo $server_output."\n";
