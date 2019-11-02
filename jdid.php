d93cf20716ec23f03d8048c8f8985a5df956c0d4
mbahtekno <57294128+mbahtekno@users.noreply.github.com>
2019-11-02 22:19:56 +0700

    Update jdid.php

--- jdid.php
+++ jdid.php
@@ -16,7 +16,7 @@ function jdidbom($no, $jum, $wait){
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($ch, CURLOPT_REFERER, 'http://sc.jd.id/phone/bindingPhone.html');
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36');
         $server_output = curl_exec ($ch);
         curl_close ($ch);
 		echo $server_output."\n";
@@ -40,7 +40,7 @@ $nomor = trim(fgets(STDIN));
 echo "Jumlah?\nInput : ";
 $jumlah = trim(fgets(STDIN));
 echo "Jeda? 0-99999 (ex:1)\nInput : ";
-$jeda = trim(fgets(STDIN));
+$jeda = '1';
 $execute = jdidbom($nomor, $jumlah, $jeda);
 print $execute;
 ?>
